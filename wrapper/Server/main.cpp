10587b9f555ff304edf74564c8ae277531868e56
TejaswiBishnoi <62018605+TejaswiBishnoi@users.noreply.github.com>
2023-01-11 23:50:27 +0530

    Socket Connection Complet

--- wrapper/Server/main.cpp
+++ wrapper/Server/main.cpp
@@ -13,11 +13,12 @@
 
 using namespace std;
 
-int PORT = 23;
-char* IP_ADDR = "192.168.4.1";
+int PORT = 6000;
+char* IP_ADDR = "127.0.0.1";
 int SockID;
 
 class Communication{
+public:
     bool ConnectSocket(){
         struct sockaddr_in sockaddr;
         SockID = socket(AF_INET, SOCK_STREAM, 0);
@@ -30,22 +31,22 @@ class Communication{
         sockaddr.sin_addr.s_addr = inet_addr(IP_ADDR);
         long arg;
         
-        if ((arg = fcntl(SockID, F_GETFL, NULL)) < 0){
-            cout << "Connection Failure!" << endl;
-            return false;
-        }
-        arg |= O_NONBLOCK;
-        if (fcntl(SockID, F_SETFL, arg) < 0){
-            cout << "Connection Failure!" << endl;
-            return false;
-        }
+        // if ((arg = fcntl(SockID, F_GETFL, NULL)) < 0){
+        //     cout << "Connection Failure!" << endl;
+        //     return false;
+        // }
+        // arg |= O_NONBLOCK;
+        // if (fcntl(SockID, F_SETFL, arg) < 0){
+        //     cout << "Connection Failure!" << endl;
+        //     return false;
+        // }
 
         int res = connect(SockID, (struct sockaddr *) &sockaddr, sizeof(sockaddr));
         if (res < 0){
-            cout << "Connection Failure!" << endl;
+            cout << "Connection Faillure!" << endl;
+            if (errno == EINPROGRESS) cout << "bb";
             return false;
         }
-
         if ((arg = fcntl(SockID, F_GETFL, NULL)) < 0){
             cout << "Connection Failure!" << endl;
             return false;
@@ -58,11 +59,54 @@ class Communication{
 
         int optval;
         socklen_t optlen;
-        if(getsockopt(SockID, SOL_SOCKET, SO_KEEPALIVE, &optval, &optlen) < 0){
-            cout << "Connection Failure!" << endl;
-            close(SockID);
-            return false;
-        }
-        
+        // if(getsockopt(SockID, SOL_SOCKET, SO_KEEPALIVE, &optval, &optlen) < 0){
+        //     cout << "Connection Failure!" << endl;
+        //     close(SockID);
+        //     return false;
+        // }
+        // optval = 1;
+        // optlen = sizeof(optval);
+        // if (setsockopt(SockID, SOL_SOCKET, SO_KEEPALIVE, &optval, optlen) < 0){
+        //     cout << "Connection Failure!" << endl;
+        //     close(SockID);
+        //     return false;
+        // }
+        // if(getsockopt(SockID, SOL_SOCKET, SO_KEEPALIVE, &optval, &optlen) < 0){
+        //     cout << "Connection Failure!" << endl;
+        //     close(SockID);
+        //     return false;
+        // }
+        // int error = 0;
+        // socklen_t len = sizeof(error);
+        // int retval = getsockopt(SockID, SOL_SOCKET, SO_ERROR, &error, &len);
+
+        // if (retval != 0){
+        //     cout << "Connection Failure!" << endl;
+        //     close(SockID);
+        //     return false;
+        // }
+        // if (error != 0){
+        //     cout << "Connection Failure!" << endl;
+        //     close(SockID);
+        //     return false;
+        // }
+        // else {
+        //     cout << "Pluto Connected" << endl;
+        // }
+        cout << "Pluto Connected" << endl;
+        return true;
     }
 };
+
+int main(){
+    Communication comm;
+    if(comm.ConnectSocket() == true){
+        char buffer[8];
+        for (int i = 0; i < 8; i++){
+            buffer[i] = 'a';
+        }
+        int n = write(SockID, buffer, strlen(buffer));
+        if (n < 0) cout << "error";
+    }
+    sleep(5);
+}
