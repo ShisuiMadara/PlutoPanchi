797cdc5b82c5a3a96388f5fefa68243809ae8abd
shisuimadara <2020ucs0090@iitjammu.ac.in>
2023-01-12 23:41:12 +0530

    completed command buffer

--- commands/command.cpp
+++ commands/command.cpp
@@ -4,14 +4,36 @@
 
 using namespace std;
 
-vector<uint8_t>command_buffer[7];
+vector<uint8_t> command_buffer;
 
-void command::sendRequestMSP_SET_COMMAND(int commandType)
-{
-  vector<int8_t> payload(1);
-  payload[0] = (int8_t) (commandType & 0xFF);
 
-  sendRequestMSP(createPacketMSP(MSP_SET_COMMAND, payload));
+void init_command_buffer () {
+	command_buffer[0] = '$';
+    command_buffer[1] = 'M';
+    command_buffer[2] = '<';
+    command_buffer[3] = (uint8_t) 1;
+    command_buffer[4] = (uint8_t) 217;
+    command_buffer[5] = (uint8_t) 0;
+
+    calcCRC(command_buffer) 
 }
+
+void* get_command_req (void* com) {
+
+	Communication* comm = (Communication*) com;
+
+	while(true) {
+
+		//call subscriber
+
+		pthread_mutex_lock(&socketLock);
+	    write(SockID, &com->command_buffer[0], com->command_buffer.size());
+	    pthread_mutex_unlock(&socketLock);
+	      
+	}
+ }
+
+
+
 
 
