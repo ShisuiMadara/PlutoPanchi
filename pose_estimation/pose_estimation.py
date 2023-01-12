5c62a5b5bfc30721c9411040173f65a89f4b2195
Av-nish <soy.avnish@gmail.com>
2023-01-12 20:33:16 +0530

    depth cam tested for distance

--- pose_estimation/pose_estimation.py
+++ pose_estimation/pose_estimation.py
@@ -1,44 +1,65 @@
 import cv2
-import cv2.aruco as aruco
+import pyrealsense2
 from realsense_depth import *
+import cv2.aruco as aruco
+
+point = (400, 300)
+
+# def show_distance(event, x, y, args, params):
+#     global point
+#     point = (x, y)
 
 # Initialize Camera Intel Realsense
 dc = DepthCamera()
-ret, depth_frame, color_frame = dc.get_frame()
+
+# Create mouse event
+# cv2.namedWindow("Color frame")
+# cv2.setMouseCallback("Color frame", show_distance)
 
 
-def detectMarker(img, location, markerSize=4, totalMarker=50, draw=True):
+def detectMarker(img, markerSize=4, totalMarker=50, draw=True):
     imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     key = getattr(aruco, f'DICT_{markerSize}X{markerSize}_{totalMarker}')
-    arucoDict = aruco.Dictionary_get(key)
+    arucoDict = aruco.getPredefinedDictionary(key)
 
-    arucoParam = aruco.DetectorParameters_create()
-    bbox, ids, rejected = aruco.detectMarkers(
-        imgGray, arucoDict, parameters=arucoParam)
-
+    arucoParam = aruco.DetectorParameters()
+    detector = aruco.ArucoDetector(arucoDict, arucoParam)
+    bbox, ids, rejected = detector.detectMarkers(imgGray,)
+    center = ()
     if draw:
         aruco.drawDetectedMarkers(img, bbox)
 
-    if ids is not None:
+    return_dict = {}
+    if bbox:
 
-        for i in range(len(ids)):
+        x = (bbox[0][0][0][0] + bbox[0][0][2][0]) // 2
+        x1 = (bbox[0][0][1][0] + bbox[0][0][3][0]) // 2
+        
+        y = (bbox[0][0][0][1] + bbox[0][0][2][1]) // 2
+        y1 = (bbox[0][0][1][1] + bbox[0][0][3][1]) // 2
 
-            coordinates = [[int(j[0]), int(j[1])] for j in bbox[i][0]]
-            coordinates.append([(int(bbox[i][0][0][0]) + int(bbox[i][0][2][0])) //
-                                2, (int(bbox[i][0][0][1]) + int(bbox[i][0][2][1]))//2])
+        center = ((x1 + x)// 2), int((y + y1)// 2)
+        # print(center)
 
-            location[ids[i][0]] = coordinates
-    return location
-
-
-def get_distance(point):
-
-    cv2.circle(color_frame, point, 4, (0, 0, 255))
-    distance = depth_frame[point[1], point[0]]
-    cv2.putText(color_frame, "{}mm".format(distance), (point[0], point[1] - 20), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 0), 2)
+    return center
 
 
-point = (400,300) #replcae this with return value of the function detect marker
 
-# Show distance for a specific point
-get_distance(point)    
+while True:
+    ret, depth_frame, color_frame = dc.get_frame()
+
+    center = detectMarker(color_frame)
+    # Show distance for a specific point
+    # print(point, center)
+
+    if center:
+        cv2.circle(color_frame, (int(center[0]), int(center[1])), 6, (0, 0, 255))
+        distance = depth_frame[int(center[1]), int(center[0])]
+        cv2.putText(color_frame, "{}mm".format(distance), (point[0], point[1] - 200), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
+        detectMarker(color_frame)
+
+    cv2.imshow("depth frame", depth_frame)
+    cv2.imshow("Color frame", color_frame)
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
