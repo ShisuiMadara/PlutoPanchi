59c703b2d3c1fc02ea8adc2a1321282243140193
Av-nish <soy.avnish@gmail.com>
2023-01-24 20:09:15 +0530

    Dist and direction from center

--- pose_estimation/pose_estimation.py
+++ pose_estimation/pose_estimation.py
@@ -4,19 +4,13 @@ from realsense_depth import *
 import cv2.aruco as aruco
 import zmq
 import time
+import math
 
 point = (400, 300)
 
-# def show_distance(event, x, y, args, params):
-#     global point
-#     point = (x, y)
 
 # Initialize Camera Intel Realsense
 dc = DepthCamera()
-
-# Create mouse event
-# cv2.namedWindow("Color frame")
-# cv2.setMouseCallback("Color frame", show_distance)
 
 publisher = None
 def publish(data):
@@ -36,7 +30,7 @@ def height(data):
 
     topic = "height"
 
-    print(stri)
+    # print(stri)
     publisher.send_string(topic, flags=zmq.SNDMORE)
     publisher.send_string(stri)
 
@@ -50,6 +44,14 @@ def detectMarker(img, markerSize=4, totalMarker=50, draw=True):
     detector = aruco.ArucoDetector(arucoDict, arucoParam)
     bbox, ids, rejected = detector.detectMarkers(imgGray,)
     center = ()
+
+    (h, w) = color_frame.shape[:2]
+    # print(h,w)
+    cv2.circle(color_frame, (w//2, h//2), 7, (255, 255, 255), -1) 
+
+    distance_from_center = None
+    head_back_to = [None, None]
+
     if draw:
         aruco.drawDetectedMarkers(img, bbox)
 
@@ -63,11 +65,25 @@ def detectMarker(img, markerSize=4, totalMarker=50, draw=True):
         y1 = (bbox[0][0][1][1] + bbox[0][0][3][1]) // 2
 
         center = ((x1 + x)// 2), int((y + y1)// 2)
-        # print(center)
+        distance_from_center = math.sqrt((center[0]- w//2)**2 + (center[1]- h//2)**2)
+        if (center[0] < w//2):
+            head_back_to[0] = 'R'
+        else:
+            head_back_to[0] = 'L'
+
+        if (center[1] < h//2):
+            head_back_to[1] = 'B'
+        else:
+            head_back_to[1] = 'F'    
+                
+            
+        print(distance_from_center)
+        print(head_back_to)
+        # print(center, w//2, h//2)
     
     publish(center)
 
-    return center
+    return center, distance_from_center, head_back_to
 
 
 
@@ -87,18 +103,19 @@ if __name__ == '__main__':
     while True:
         ret, depth_frame, color_frame = dc.get_frame()
 
-        center = detectMarker(color_frame)
+        center = detectMarker(color_frame)[0]
         # Show distance for a specific point
         # print(point, center)
 
         if center:
             cv2.circle(color_frame, (int(center[0]), int(center[1])), 6, (0, 0, 255))
             distance = depth_frame[int(center[1]), int(center[0])]
-            height(distance)
-           
+            height(distance)  
+
             cv2.putText(color_frame, "{}mm".format(distance), (point[0], point[1] - 200), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
             detectMarker(color_frame)
 
+        
         cv2.imshow("depth frame", depth_frame)
         cv2.imshow("Color frame", color_frame)
         key = cv2.waitKey(1)
